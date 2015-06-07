7f7477f247b93c03c20842ffc1f0c5962cddf28f
Jesper Boldt <jesper@bboldt.dk>
2015-06-07 21:48:11 +0200

    update

--- autoRunReport.m
+++ autoRunReport.m
@@ -1,4 +1,5 @@
 beep off
+!git clone https://github.com/jbboldt/stocks.git C:\Users\Jesper Boldt\Documents\matlab\projects\stocks
 cd( 'C:\Users\Jesper Boldt\Documents\matlab\projects\stocks' )
 fclose all
 [s,m,mi] = rmdir('reports');
