cec01bb2f2484e9a65178e8288e8234c02c4202e
boldt <jesper@bboldt.dk>
2015-06-09 23:06:20 +0200

    dff:xxx.dff:xxx

--- autoRunReport.m
+++ autoRunReport.m
@@ -1,6 +1,4 @@
 beep off
-[s,m,mi] = rmdir('C:\Users\Jesper Boldt\Documents\matlab\projects\stocks');
-!git clone https://github.com/jbboldt/stocks.git "C:\Users\Jesper Boldt\Documents\matlab\projects\stocks"
 cd( 'C:\Users\Jesper Boldt\Documents\matlab\projects\stocks' )
 fclose all
 [s,m,mi] = rmdir('reports');
