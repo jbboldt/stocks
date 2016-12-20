3be6a5bf51f264ed75ee79ecad627c38c88af10f
Jesper Boldt <jesper@bboldt.dk>
2016-12-20 20:19:08 +0100

    upd

--- autoRun.m
+++
@@ -1,6 +0,0 @@
-
-cd( 'C:\matlab\projects\stocks' )
-
-makeReport
-
-exit
--- copy2lenovo.m
+++
@@ -1,3 +0,0 @@
-copyfile( 'functions', '\\LENOVO\Users\Jesper Boldt\Documents\matlab\projects\stocks\functions' );
-copyfile( 'autoRunReport.m', '\\LENOVO\Users\Jesper Boldt\Documents\matlab\projects\stocks' );
-copyfile( 'C:\matlabTool', '\\LENOVO\Users\Jesper Boldt\Documents\matlab\tools' );
